06623ef448a7c345c430e50dbe17a847497c0aea
VividerAphid <trever.kress@gmail.com>
2026-07-07 17:31:24 -0400

    Favicon + data loading tests

--- main.js
+++ main.js
@@ -17,5 +17,21 @@ async function getItemListings() {
 }
 
 function updateUI(){
-    console.log(data);
+    //console.log(data);
+    for(plant in data.plants){
+      let tile = addElement(plant+"Tile", "div", document.getElementById("testInfo"), data.plants[plant].name);
+      let img = addElement(plant+"Img", "img", document.getElementById(plant+"Tile"));
+      img.src = "/images/" + data.plants[plant].image;
+      img.width = 200;
+      img.height = 200;
+    }
+   
+}
+
+function addElement(id, type, parent, innards){
+	var child = document.createElement(type);
+	child.id = id;
+	parent.appendChild(child);
+	child.innerHTML = innards || "";
+    return child;
 }
