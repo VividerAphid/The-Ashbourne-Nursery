a8ad14e650990b8b7ed38091beb014f92bd86cad
VividerAphid <trever.kress@gmail.com>
2026-07-13 22:34:53 -0400

    Added display of all products, additional formatting

--- main.js
+++ main.js
@@ -17,18 +17,28 @@ async function getItemListings() {
 }
 
 function updateUI(){
-    //console.log(data);
     for(plant in data.plants){
-      // let tile = addElement(plant+"Tile", "div", document.getElementById("testInfo"), data.plants[plant].name);
-      // let img = addElement(plant+"Img", "img", document.getElementById(plant+"Tile"));
-      // img.src = "/images/" + data.plants[plant].image;
-      // img.width = 200;
-      // img.height = 200;
       let cardInfo = {title: data.plants[plant].name, subName: data.plants[plant].subName, 
                 image: data.plants[plant].image, 
                 description: data.plants[plant].description, 
                 parent: document.getElementById("testInfo")};
-      //console.log(cardInfo);
+      buildCard(cardInfo);
+    }
+    for(supply in data.supplies){
+      let cardInfo = {title: data.supplies[supply].name, 
+                subName: "",
+                image: data.supplies[supply].image, 
+                description: "",
+                parent: document.getElementById("testInfo")};
+      buildCard(cardInfo);
+    }
+    for(extra in data.extra){
+      let cardInfo = {title: data.extra[extra].name, 
+                subName: "",
+                image: data.extra[extra].image, 
+                description: "",
+                price: (data.extra[extra].price) ? data.extra[extra].price : undefined,
+                parent: document.getElementById("testInfo")};
       buildCard(cardInfo);
     }
    
@@ -47,11 +57,13 @@ function addElement(properties){
 function buildCard(props){
     //Takes in title, desc, parent
     //console.log(props);
+    let price = (props.price) ? props.price : "$0";
     let currentCard = addElement({type: "div", id: props.title+"Card", class: "card", parent: props.parent, innards:""});//Card body
     addElement({type:"div", id: props.title + "-title", class:"cardTitle", parent: currentCard, innards:props.title});//Card title
-    addElement({type:"div", id: props.title + "-title", class:"cardSubname", parent: currentCard, innards: "<i>"+props.subName+"</i>"});//Card subname
+    addElement({type:"div", id: props.title + "subname", class:"cardSubname", parent: currentCard, innards: "<i>"+props.subName+"</i>"});//Card subname
     let cardImg = addElement({type:"img", id: props.title + "-picture", class:"cardPicture", parent:currentCard});//Card picture
     cardImg.src = "/images/" + props.image;
     addElement({type:"div", id: props.title + "-description", class:"cardDescription", parent:currentCard, innards:props.description});//Card info
+    addElement({type:"div", id: props.title + "-price", class:"cardPrice", parent:currentCard, innards:price});//Card price
     return currentCard;
 }
